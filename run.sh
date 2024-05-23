e6e9af2142c6d657f3091b64a6caa1de6f98fa1b
20230108work <a1354243@gamil.com>
2024-05-23 13:02:46 +0800

    1.opt.

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@ curl -L -o ~/wrk https://github.com/20230108work/tools/raw/main/wrk
 chmod +x ~/wrk
 
 curl -L -o ~/limit.1.rua https://github.com/20230108work/tools/raw/main/limit.1.rua
+curl -L -o ~/get.rua https://github.com/20230108work/tools/raw/main/get.rua
